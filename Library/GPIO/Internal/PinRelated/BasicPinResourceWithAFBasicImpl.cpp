e35b9162134d165183c677f8a4f90e385a3fd00c
tobilappi <egtobi@gmail.com>
2020-08-03 03:17:26 +0200

    Modularized GPIO?

--- Library/GPIO/Internal/PinRelated/BasicPinResourceWithAFBasicImpl.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by TobiEgger on 17.02.2020.
-//
-
-#include "BasicPinResourceWithAFBasicImpl.h"
-
-BasicPinResourceWithAFBasicImpl::af_t BasicPinResourceWithAFBasicImpl::getAF() const {
-	return af;
-}
